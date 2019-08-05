38a48611cbd6239362b088b0ae5e765e8c542cfc
Yunyu Lin <mail@yunyul.in>
2019-08-05 09:30:40 -0700

    Add submitting state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import { Box, Grommet, Heading, Paragraph } from 'grommet';
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import Form from './Form.jsx';
+import hourglass2 from './hourglass2.png';
 import { MLHBadge } from './MLHBadge';
 import { desktopCss } from './utils.jsx';
-import hourglass2 from './hourglass2.png';
-import { Grommet } from 'grommet';
 
 const Container = styled.div`
   padding: 36px;
@@ -47,7 +47,17 @@ const App = () => (
     <MLHBadge />
     <Container>
       <HourglassImage />
-      <Form />
+      <Box>
+        <Heading level={1} size="large" margin={{ bottom: 'small' }}>
+          HackDuke
+        </Heading>
+        <Paragraph size="large">Code for Good with us in Durham, NC</Paragraph>
+        <Paragraph size="large" margin={{ bottom: 'medium' }}>
+          November 2-3rd, 2019
+        </Paragraph>
+
+        <Form />
+      </Box>
     </Container>
   </Grommet>
 );
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,9 @@
 import { validate } from 'email-validator';
-import { Box, Button, FormField, Heading, Paragraph, TextInput } from 'grommet';
+import { Box, Button, FormField, TextInput, Text } from 'grommet';
 import React, { useMemo, useState } from 'react';
 import { useDebounce } from 'use-debounce';
-import { getUniversitySuggestions, isUniversity } from './university-util.js';
+import { getUniversitySuggestions, isUniversity } from './university-util';
+import { Spinner } from './Spinner';
 
 const Form = () => {
   const [name, setName] = useState('');
@@ -11,53 +12,60 @@ const Form = () => {
   const [debouncedSchool] = useDebounce(school, 50, { maxWait: 250 });
   const isValid = name.length >= 3 && validate(email) && isUniversity(school);
 
+  const [submitting, setSubmitting] = useState(false);
+
   return (
-    <Box>
-      <Heading level={1} size="large" margin={{ bottom: 'small' }}>
-        HackDuke
-      </Heading>
-      <Paragraph size="large">Code for Good with us in Durham, NC</Paragraph>
-      <Paragraph size="large" margin={{ bottom: 'medium' }}>
-        November 2-3rd, 2019
-      </Paragraph>
-      <Box background="white" pad="medium" round="small" responsive={false} elevation="medium">
-        <FormField label="Name">
-          <TextInput
-            placeholder="First Last"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-        </FormField>
-        <FormField label="Email">
-          <TextInput
-            placeholder="me@example.com"
-            value={email}
-            type="email"
-            onChange={e => setEmail(e.target.value)}
-          />
-        </FormField>
-        <FormField label="School">
-          <TextInput
-            placeholder="Duke University"
-            suggestions={useMemo(
-              () => getUniversitySuggestions(debouncedSchool),
-              [debouncedSchool]
-            )}
-            value={school}
-            onChange={e => setSchool(e.target.value)}
-            onSelect={e => setSchool(e.suggestion)}
-          />
-        </FormField>
-        <Button
-          disabled={!isValid}
-          label="NOTIFY ME"
-          primary={true}
-          type="submit"
-          fill="horizontal"
-          margin={{ top: 'small' }}
-          color="#5052FF"
+    <Box
+      background="white"
+      pad="medium"
+      round="small"
+      responsive={false}
+      elevation="medium"
+    >
+      <FormField label="Name">
+        <TextInput
+          placeholder="First Last"
+          value={name}
+          onChange={e => setName(e.target.value)}
         />
-      </Box>
+      </FormField>
+      <FormField label="Email">
+        <TextInput
+          placeholder="me@example.com"
+          value={email}
+          type="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+      </FormField>
+      <FormField label="School">
+        <TextInput
+          placeholder="Duke University"
+          suggestions={useMemo(
+            () => getUniversitySuggestions(debouncedSchool),
+            [debouncedSchool]
+          )}
+          value={school}
+          onChange={e => setSchool(e.target.value)}
+          onSelect={e => setSchool(e.suggestion)}
+        />
+      </FormField>
+      <Button
+        disabled={!isValid || submitting}
+        label={
+          submitting ? (
+            <Box direction="row" gap="xsmall" justify="center">
+              <Spinner /> <Text size="medium"> SUBMITTING... </Text>
+            </Box>
+          ) : (
+            'NOTIFY ME'
+          )
+        }
+        onClick={() => setSubmitting(true)}
+        primary={true}
+        type="submit"
+        fill="horizontal"
+        margin={{ top: 'small' }}
+      />
     </Box>
   );
 };
